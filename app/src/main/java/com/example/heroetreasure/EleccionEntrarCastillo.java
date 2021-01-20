f650271786386bc56f9ebd2b4ff0f2ea84fc9660
IamJosito <joserr0209@gmail.com>
2021-01-20 19:07:49 +0100

    Mirar libreta para ver que falta

---
+++ app/src/main/java/com/example/heroetreasure/EleccionEntrarCastillo.java
@@ -0,0 +1,14 @@
+package com.example.heroetreasure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EleccionEntrarCastillo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eleccion_entrar_castillo);
+    }
+}
---
+++ app/src/main/java/com/example/heroetreasure/PelearSoldado.java
@@ -0,0 +1,14 @@
+package com.example.heroetreasure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PelearSoldado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pelear_soldado);
+    }
+}
